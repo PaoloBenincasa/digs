3191dea0a505e06e32c3839e1e4fcee882de7492
Paolo Benincasa <paolobenincasa1984@gmail.com>
2024-10-25 18:55:36 +0200

    scaffolding

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{route('homepage')}}">Digs</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="{{route('homepage')}}" class="nav-link" aria-current="page">Home</a"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
